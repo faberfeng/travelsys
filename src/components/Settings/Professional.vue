699783b2c98fb0b0733b424d95b0097ee41caf8a
DAVID <fwc.david@qq.com>
2018-04-09 14:05:01 +0800

    #7 专业工种分类编码

--- src/components/Settings/Professional.vue
+++ src/components/Settings/Professional.vue
@@ -7,21 +7,21 @@
         <div class="worktable">
          <zk-table 
              index-text="序号"
-            :data="codingList" :columns="columns" :tree-type="props.treeType" 
+            :data="codingList" :columns="columns" :tree-type="props.treeType"
             :expand-type="props.expandType" :show-index="props.showIndex" :selection-type="props.selectionType" 
             :border="props.border" >
                 <template slot="action" slot-scope="scope">
-                   <button class=""   v-if="scope.row.status == 0" @click="edit(scope)">提请</button>
-                    <button class=""   v-if="scope.row.status == 1" @click="edit(scope)">通过</button>
-                     <button class=""   v-if="scope.row.status == 1" @click="edit(scope)">退回</button>
-                   <button class="editBtn actionBtn" @click="edit(scope)" v-if="scope.row.status == 2 || scope.row.status == 0"></button>
-                   <button class="deleteBtn actionBtn" @click="deleteItem(scope.rowIndex)" v-if="scope.row.status == 2 || scope.row.status == 0"></button>
+                   <button class="TiqingBtn actionBtn" title="提请"  v-if="scope.row.status == 0" @click="request(scope)"></button>
+                    <button class="passBtn actionBtn" title="通过"   v-if="scope.row.status == 1" @click="pass(scope)"></button>
+                     <button class="backBtn actionBtn" title="退回"   v-if="scope.row.status == 1" @click="reject(scope)"></button>
+                   <button class="editBtn actionBtn" title="修改"  @click="edit(scope)" v-if="scope.row.status == 2 || scope.row.status == 0"></button>
+                   <button class="deleteBtn actionBtn" title="删除"  @click="deleteItem(scope)" v-if="scope.row.status == 2 || scope.row.status == 0"></button>
                 </template> 
             </zk-table>
         </div>
     </div>
       
-      <div id=edit>
+      <div id='edit'>
         <el-dialog title="编辑编码" :visible.sync="addCode" :before-close="userClose">
             <div class="editBody">
                 <div class="editBodyone edit-item clearfix">
@@ -61,6 +61,29 @@
                 <button class="editBtnC" @click="userClose">取消</button>
             </div>
         </el-dialog>
+        <el-dialog class="confirm"
+        :visible.sync="confirmVisible"
+        :before-close="confirmClose">
+            <span class="icon-confirm"></span>
+            <span class="title-confirm">删除提醒</span>
+            <span class="text-confirm">确认删除本条分类编码？<br>
+            删除后将无法撤销。</span>
+             <div  class="dialog-footer">
+                <button class="editBtnS" @click="deleteCode">删除</button>
+                <button class="editBtnC" @click="confirmClose">取消</button>
+            </div>
+        </el-dialog>
+        <el-dialog class="confirm"
+        :visible.sync="confirmVisible_1"
+        :before-close="remindClose">
+            <span class="icon-confirm icon-request"></span>
+            <span class="title-confirm" v-text="confirm.title"></span>
+            <span class="text-confirm" v-html="confirm.msg"></span>
+             <div  class="dialog-footer">
+                <button class="editBtnS" @click="deleteCode">删除</button>
+                <button class="editBtnC" @click="remindClose">取消</button>
+            </div>
+        </el-dialog>
     </div>
   </div>
 </template>
@@ -78,13 +101,11 @@ export default {
             token:'',
             projId:'',
             baseUrl:'http://10.252.26.240:8080/h2-bim-project/',
-            defaultProps:{
-              children: 'children',
-               label: 'number'
-            },
             addCode:false,
-             props: {
-                    stripe: false,
+            confirmVisible:false,
+            confirmVisible_1:false,
+            props: {
+                    stripe: true,
                     border: true,
                     showHeader: true,
                     showSummary: false,
@@ -99,30 +120,36 @@ export default {
                 {
                     label: '编码',
                     prop: 'number',
-                    width: '200px',
+                    minWidth: '260px',
                 },
                 {
                     label: '标题',
                     prop: 'title',
-                    minWidth: '50px',
+                    width: '185px',
                 },
                 {
                     label: '来源',
                     prop: 'type_',
+                    width: '205px',   
                 },
                 {
                     label: '状态',
                     prop: 'status_',
-                    minWidth: '200px',
+                    width: '115px',
                 },
                 {
                     label:'操作',
                     prop:'operator',
                     type: 'template',
                     template: 'action',
-                    width:'100px'
+                    minWidth:'125px'
                 }
             ],
+            deleteWorkCode:{},//打算删除的workcode
+            confirm:{
+                msg:'',
+                title:''
+            }
         }
     },
     created(){
@@ -136,6 +163,50 @@ export default {
       
     },
     methods:{
+        request(rows){//提请 status为0时
+        // var projectId = window.parent.BIM.Tool.getCurrentProjId();
+        // var row = $('#treegrid').treegrid('find', id); // reload the all rows
+        // var parRow = $('#treegrid').treegrid('getParent', id);
+        // if(parRow!=null){
+        //     if(parRow.status==0||parRow.status==2){
+        //         $.messager.alert("警告", "该编码的所有父编码必须为【已提请】状态，才能提请", "warning");
+        //         return;
+        //     }
+        //     var parRow2 = $('#treegrid').treegrid('getParent', parRow.id);
+        //     if(parRow2!=null){
+        //         if(parRow2.status==0||parRow2.status==2){
+        //             $.messager.alert("警告", "该编码的所有父编码必须为【已提请】状态，才能提请", "warning");
+        //             return;
+        //         }
+        //         var parRow3 = $('#treegrid').treegrid('getParent', parRow2.id);
+        //         if(parRow3!=null){
+        //             if(parRow3.status==0||parRow3.status==2){
+        //                 $.messager.alert("警告", "该编码的所有父编码必须为【已提请】状态，才能提请", "warning");
+        //                 return;
+        //             }
+        //         }
+        //     }
+        // }
+            var vm = this
+            vm.confirmVisible_1 = true
+            vm.confirm.title = '确认提请'
+            vm.confirm.msg = '确认提请本条分类编码？<br>处于提请状态时将无法删除。'
+        },
+        pass(rows){//通过 status为1时
+
+        },
+         reject(rows){//退回 status 为1时
+
+        },
+        remindClose(){
+            var vm = this
+            vm.confirmVisible_1 = false
+        },
+        confirmClose(){
+            var vm = this
+            vm.confirmVisible = false
+            vm.deleteWorkCode = {}
+        },
         initCode(key){
             if(!key)return false;
             var vm = this
@@ -239,6 +310,39 @@ export default {
         //删除
         deleteItem(num){
             console.log(num)
+            var vm = this
+            vm.confirmVisible = true
+            vm.deleteWorkCode = num.row
+        },
+        deleteCode(){
+             var vm = this
+            axios({
+                method:'POST',
+                url:'http://10.252.26.240:8080/h2-bim-project/config2/component/deleteWorkCode',
+                headers:{
+                    'token':vm.token
+                },
+                data:vm.deleteWorkCode,
+                params:{
+                    projectId:vm.projId,
+                }
+            }).then((response)=>{
+                if(response.data.cd == 40001){
+                    vm.$message({
+                        type:'error',
+                        message:response.data.msg
+                    })
+                }else{
+                     vm.$message({
+                        type:'success',
+                        message:response.data.msg
+                    })
+                vm.deleteWorkCode = {}
+                vm.confirmVisible = false
+                } 
+            }).catch((err)=>{
+                console.log(err)
+            })
         },
         formatterStatus(value, row, index) {
             if (row.type == 0 || row.type == 1) {
@@ -292,7 +396,7 @@ export default {
                 var levelNum = {}
                 for(var i=0;i<arr.length;i++){
                     arr[i].type_ = vm.formatterType(arr[i].type)
-                    arr[i].status_ = vm.formatterStatus(arr[i].id,arr[i])
+                    arr[i].status_ = vm.formatterStatus(arr[i].status,arr[i])
                     //根据level截取number
                     switch(arr[i].level){
                         case 1:
@@ -375,6 +479,49 @@ export default {
             white-space: nowrap;
         }
     }
+    .zk-table{
+        color: #333333;
+    }
+    .zk-table--tree-icon{
+        position: relative;
+            width: 40px;
+    display: inline-block;
+    z-index: 10;
+    background: #ffffff;
+    }
+    .zk-table--row-hover .zk-table--tree-icon{
+ background: #ebf7ff;
+    }
+    .zk-table--tree-icon::after {
+       display: block;
+       position: absolute;
+        top: 6px;
+        left: 20px;
+       width: 15px;
+       height: 12px;
+       background:url('./images/folder_1.png')no-repeat 0 0; 
+       content: '';
+    }
+    .zk-icon-minus-square-o::after{
+         background:url('./images/folder.png')no-repeat 0 0; 
+    }
+    .zk-table__body-row>td:first-of-type{
+        width: 45px;
+    }
+    .zk-table--level-4-cell,.zk-table--level-3-cell,.zk-table--level-2-cell,.zk-table--level-1-cell,.zk-table--level-5-cell{
+      position: relative;
+     }
+    .zk-table--level-4-cell::before,.zk-table--level-3-cell::before,.zk-table--level-2-cell::before,.zk-table--level-1-cell::before,.zk-table--level-5-cell::before{
+        display: block;
+       position: absolute;
+        top: 2px;
+         left: 2px;
+       width: 12px;
+       height: 14px;
+       background:url('./images/file.png')no-repeat 0 0; 
+       content: '';
+       z-index: 1;
+    }
     .el-dialog{
         left: 50%;
         width: 586px!important;
@@ -422,6 +569,7 @@ export default {
         height: 16px;
         border: none;
         cursor: pointer;
+        margin-right: 16px;
     }
     .zk-table__cell-inner {
     padding: 6px 12px;
@@ -431,7 +579,15 @@ export default {
     }
     .deleteBtn{
         background: url('../../assets/delete.png') no-repeat;
-        margin-left: 20px;
+    }
+    .TiqingBtn{
+          background: url('./images/tiqing.png') no-repeat;
+    }
+    .passBtn{
+        background: url('./images/pass.png') no-repeat;
+    }
+     .backBtn{
+        background: url('./images/back.png') no-repeat;
     }
     .el-tree-node__content{
         height: auto;
@@ -568,6 +724,54 @@ export default {
     }
     .btn i{
         margin-right: 10px;
+    }
+    .confirm{
+        .el-dialog{
+            width: 398px!important;
+            margin-left: -199px;
+            border-radius: 4px;
+        }
+         .el-dialog__header{
+            height: 40px; 
+            padding: 0; 
+            border-bottom: none; 
+            text-align: left; 
+        }
+        .el-dialog__body{
+            margin-top: 0;
+            padding-bottom:33px; 
+        }
+        .icon-confirm{
+            display: block;
+            width: 50px;
+            margin: 0 auto;
+            height: 50px;
+            background:url('./images/remind.png') no-repeat 0 0; 
+        }
+        .title-confirm{
+            margin: 20px 0 15px;
+            color: #fc3439;
+            font-weight: bold;
+            font-size: 18px;
+            text-align: center;
+            display: block;
+        }
+        .icon-request{
+             background:url('./images/prompt.png') no-repeat 0 0; 
+        }
+        .text-confirm{
+            display: block;
+            font-size: 14px;
+            text-align: center;
+            color: #333333;
+            line-height: 16px;
+            margin-bottom: 30px;
+        }
+        .dialog-footer{
+            button{
+                border-radius: 2px;
+            }
+        }
     }
 }
 </style>
