00fa0ed48df3a7da6acd4df964a8e2f2b90e502d
licongwen <649091795@qq.com>
2018-07-20 17:56:30 +0800

    修改z-index层级bug

--- src/components/planCost/quantities.vue
+++ src/components/planCost/quantities.vue
@@ -822,9 +822,6 @@ export default {
         vm.BDMSUrl = vm.$store.state.BDMSUrl;
         vm.getInentityDetail();
     },
-    beforeDestroy(){
-        $('.navigation').css('z-index','1000');
-    },
     watch:{
         'pageDetial.currentPage':function(val,oldval){
             var vm = this
