c60c6c580738e3c7e9a899f8012f6d0b50bc9197
5600-1C131华建数创云工程事业部S-封伟波 <24348@xd-ad.com.cn>
2018-08-14 15:32:59 +0800

    现场连线全景模型

--- src/components/constructionSite/fieldConnection.vue
+++ src/components/constructionSite/fieldConnection.vue
@@ -23,7 +23,7 @@
                                 <span class="fullScreen" @click="fullModule"></span>
                             </div>
                             <div class="video_body">
-                                <iframe id="mm" style="width: 100%;height:calc(100%);border:0;" ></iframe>
+                                <iframe id="mm" style="width: 100%;height:calc(100%);border:0;" src="https://site.altizure.cn/s/ryLw2SAxX" ></iframe>
                                 
                                     <!-- <iframe style="width: 100%;height:100%"  src="../../../webGL/index.html"></iframe> -->
                                 
