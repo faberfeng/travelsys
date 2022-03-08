3507d03dd55216b0569d5a9f82f87d5879e76e46
faberfeng <992068956@qq.com>
2022-03-08 11:22:30 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,62 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueAMap from 'vue-amap';
 import App from './App'
 import router from './router'
 import store from '@/utils/store.js';
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import '../theme/index.css'
-import ZkTable from 'vue-table-with-tree-grid'
-import vuescroll from 'vue-scroll'
-import FullCalendar from 'vue-full-calendar'
-import './directives'
-import 'jquery'
-import 'uikit'
-// import "uikit-css"
-// import 'uikit/dist/css/uikit.almost-flat.css'
-
-// import UIkit from './uikit'
-
-// Vue.use(UIkit)
-
-// import htmlToPdf from '@/components/utils/htmlToPdf.js'  
-// Vue.use(htmlToPdf) 
-import '../node_modules/fullcalendar/dist/fullcalendar.css';
-// import {Alert, Confirm, Toast} from 'wc-messagebox'
-// import 'wc-messagebox/style.css'
- 
-// Vue.use(Alert, options)
-// Vue.use(Confirm, options)
-// Vue.use(Toast, options)
-
-// require('videojs-contrib-hls/dist/videojs-contrib-hls');
-// import { lazyAMapApiLoaderInstance } from 'vue-amap';
-import 'v2-datepicker/lib/index.css';
-
-import V2Datepicker from 'v2-datepicker';
-Vue.use(V2Datepicker)
-
-Vue.use(VueAMap);
-// console.log(Vue);
-// console.log(VueAMap);
-VueAMap.initAMapApiLoader({
-  key: 'eb4e26a19ca36911be75f2f062bee6aa',
-  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor','MapType'],
-  // 默认高德 sdk 版本为 1.4.4
-  v: '1.4.4'
-});
-
-Vue.use(vuescroll)
-Vue.use(FullCalendar)
-//Vue.use(ZkTable)
-
-Vue.use(ZkTable)
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
-
 /* eslint-disable no-new */
 var app = new Vue({
   el: '#app',
---
+++ src/views/Review.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- style="overflow: hidden; left: 20px" -->
+  <div id="drawingPic" style="overflow: hidden; left: 20px">
+    <picView
+      ref="pic"
+      @load_points="loadPic"
+      @finish="drawFinish"
+      @status_changed="picView_status_changed"
+      :para="paraList"
+    ></picView>
+  </div>
+</template>
+
+<script>
+import picView from "@/components/picView.vue";
+export default {
+  name: "review",
+  components: {
+    picView,
+  },
+  data() {
+    return {
+      paraList: {},
+    };
+  },
+  create() {
+    this.paraList = {
+      type: "png",
+      source: "/static/images/12.jpg",
+      angle: 0,
+    };
+  },
+  mounted() {},
+  methods: {
+    loadPic() {
+      this.$refs.pic.Max_Select = 1;
+    },
+    drawFinish() {},
+    picView_status_changed() {},
+  },
+};
+</script>
+
+<style>
+#drawingPic {
+  margin: 0 auto;
+  overflow: auto;
+  position: absolute;
+  top: 0px;
+  left: 0;
+  bottom: 0;
+  right: 25px;
+  min-width: 1000px;
+  max-width: 1600px;
+  /* height: 800px;
+  width: 800px; */
+  border: 1px solid blue;
+
+  /* #drawPic {
+    min-width: 700px;
+    min-height: 500px;
+  }  */
+}
+</style>
